c77fece9523199d5958d07ad976c08dd56dc3273
Vitor Costa Jose <vitor.costa@prontmed.com>
2021-05-20 18:58:24 -0300

    add: onClick fn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import worldService from "./services/world";
 
 function App() {
   const countries = worldService.getCountries();
+  let [activeTab, setActiveTab] = useState("country");
+
+  function onClickTab(event) {
+    setActiveTab(event.target.id)
+  }
 
   return (
     <main className="container">
+      <section>
+        <ul className="tabs">
+          <li id="country" onClick={onClickTab}>
+            Paises
+          </li>
+          <li id="language" onClick={onClickTab}>
+            Idiomas
+          </li>
+          <li id="city" onClick={onClickTab}>
+            Cidades
+          </li>
+        </ul>
+      </section>
       <div className="country-wrapper">
         {countries.map(function (country) {
-          return <Card country={country} />;
+          return <Card key={country.Name} country={country} />;
         })}
       </div>
     </main>
